7c16b3de0804b0522056b3f2ea2773e4f05770b7
ttdesign <tkthedesigner@gmail.com>
2020-04-09 19:49:31 -0700

    add game logic and function

--- script.js
+++ script.js
@@ -4,17 +4,16 @@
 //game rules
 //show image of cards
 //user press play then cards flip down, 5 second timer/delay
-//users click- card flip up, check for match, if there's a match add to score board if card is not match deduct 1 point, max point each game =10
+//users click- card flip up, check for match, if there's a match add to score board max point each game =100
 //if all cards flipped game end
 //add restart game button add restart score board
-//shuffle cards' place
 
 //silver goal
-// card flip back if pair is not match
-//if pair is match, no flip down
+//card flip back if pair is not match
+//if pair is match, keep them up, add message if pair is not match deduct 5 point,
 
 //gold  goal
-//more cards, shuffle while play
+//more cards, shuffle while play, save favorite card
 
 //create variable that contains all cards
 let deck = [
@@ -42,7 +41,7 @@ let deck = [
 
 //start game with board full of images of cards
 let gameBoard = document.querySelector('.game-board');
-
+let myFavoriteCards = document.querySelector('.favorite-board');
 function gameStart() {
 	for (let i = 0; i < deck.length; i++) {
 		let cardFront = document.createElement('img');
@@ -70,11 +69,10 @@ function letsPlay() {
 		gameBoard.appendChild(backCard);
 	}
 }
-
-//create const to target the restart button
+//create variable to target the restart button
 const restartBtn = document.querySelector('.restart');
 
-//add event handler for the reset button
+//add event handler for the restart button
 restartBtn.addEventListener('click', restartGame);
 
 //clear the board game
@@ -94,75 +92,60 @@ function restartGame() {
 	//clearBoard();
 	//gameStart()
 }
+//create variable to target all images in the game board
+let cardsBoard = document.getElementsByTagName('img');
 
-let allCards = document.getElementsByTagName('img');
-console.log(allCards);
-console.log();
-//allCards[0].setAttribute('src', 'images/card-back.png')
 //event handler to target individual card when card is clicked
 gameBoard.addEventListener('click', flipCard);
-let input = parseInt(allCards[0].dataset.number);
+
 function flipCard(event) {
 	let userInput = parseInt(event.target.dataset.number);
 	if (event.target.classList.contains('box')) {
 		let currentCardImage = event.target.getAttribute('src');
 		let currentCardName = deck[userInput].name;
-		console.log(event.target);
+		let addButton = document.createElement('button');
+		addButton.setAttribute('class', 'add-button');
+		addButton.innerText = 'Add';
+		addButton.style.background = 'red';
+		addButton.style.position = 'relative';
+		addButton.style.zIndex = '3';
+		console.log(cardsBoard[0]);
+		cardsBoard[1].append(addButton);
+		//push card's image to card-in-play array to check matching pair
 		cardInPlay.push(currentCardName);
-		console.log(cardInPlay);
-
 		//if card is back then flip to front
 		if (currentCardImage === 'images/card-back.png') {
 			event.target.setAttribute('src', deck[userInput].image);
-			// if card is already flipped click again will check for matching
+			//myFavoriteCards.appendChild(addFavorite)
+			// if card is already flipped click again will flip it back again
 		} else {
 			event.target.setAttribute('src', 'images/card-back.png');
 		}
 	}
-	//input = allCards[0].dataset.number
-	checkIfMatch(userInput);
-	checkGame();
-	console.log(input);
-	//return input;
+	checkIfMatch(userInput); // check for matching pair
+	checkGame(); // check if game is active
 }
-console.log(allCards[0].src);
-console.log(input);
 
-// function flipBack(input) {
-//     console.log(input)
-//     allCards[2].setAttribute('src', 'images/card-back.png');
-//     //return allCards[input]
-// }
 //setTimeout(letsPlay, 4000);
-//function to check if the pair is match
-//if the second card image
+
+//function to check if the pair is matched
+//create variable to target score board and a variable to hold score
+let scoreBoard = document.querySelector('.score');
+let score = 0;
 function checkIfMatch(userInput) {
-	console.log(allCards[userInput].getAttribute('src'));
-	console.log(cardInPlay.length);
-	console.log(deck[userInput].image);
-	console.log(cardInPlay[0], cardInPlay[1]);
 	if (cardInPlay.length === 2) {
 		if (cardInPlay[0] === cardInPlay[1]) {
-			//if pair is matched, push to matched card array, reset cardsInplay array
+			//if pair is matched, push them to the matched-card array, reset card-in-play array
 			matchedCard.push(cardInPlay[0], cardInPlay[1]);
 			cardInPlay = [];
-			console.log(matchedCard, cardInPlay);
+			score += 10;
+			scoreBoard.innerText = 'Score: 00' + score;
 		} else if (cardInPlay[0] !== cardInPlay[1]) {
-			// if pair is not match, remove the last click item
-			//allCards[userInput].removeAttribute('src')
-			//setTimeout(flipBack, 2000);
-			//console.log(gameBoard[0]);
-			console.log(allCards);
-			console.log(allCards[0]);
+			// if pair is not match, remove the last clicked item from card-in-play array
 			cardInPlay.pop();
-			console.log(matchedCard);
-			console.log(cardInPlay);
-			console.log(allCards[userInput].src);
-
-			console.log(allCards[userInput].src);
-			//allCards[userInput].removeAttribute('src')
+			// if pair is not matched flip the last clicked card back down after 2 seconds
 			setTimeout(function () {
-				allCards[userInput].setAttribute('src', 'images/card-back.png');
+				cardsBoard[userInput].setAttribute('src', 'images/card-back.png');
 			}, 2000);
 		}
 	}
@@ -170,7 +153,9 @@ function checkIfMatch(userInput) {
 
 //add function to check if the game is over
 function checkGame() {
+	let winningMessage = document.querySelector('.win');
 	if (matchedCard.length === 20) {
+		winningMessage.classList.toggle('game-over');
 		console.log('game over');
 	} else {
 		return;
